6bef7393a6d8d49ce9c919ecd371e96963c7cad4
Derrick Pallas <derrick@pallas.us>
2014-04-19 12:10:54 -0700

    try: throw strerror instead of function name

--- try.h
+++ try.h
@@ -2,25 +2,26 @@
 #define TRY_H
 
 #include <cstdio>
+#include <cstring>
 #include <stdexcept>
 
 #include <errno.h>
 
 #define TRY(f, ...) ({ \
   typeof (f(__VA_ARGS__)) _r = f(__VA_ARGS__); \
-  if (_r < 0) perror(#f), throw std::runtime_error(#f); \
+  if (_r < 0) perror(#f), throw std::runtime_error(strerror(errno)); \
   _r; \
 })
 
 #define TRY_ERR(e, f, ...) ({ \
   typeof (f(__VA_ARGS__)) _r = f(__VA_ARGS__); \
-  if (_r < 0 && errno != e) perror(#f), throw std::runtime_error(#f); \
+  if (_r < 0 && errno != e) perror(#f), throw std::runtime_error(strerror(errno)); \
   _r; \
 })
 
 #define TRY_PTR(f, ...) ({ \
     typeof (f(__VA_ARGS__)) _r = f(__VA_ARGS__); \
-    if (!_r) perror(#f), throw std::runtime_error(#f); \
+    if (!_r) perror(#f), throw std::runtime_error(strerror(errno)); \
     _r; \
 })
 
