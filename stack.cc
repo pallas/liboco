4993045ad727d7400982f6ea38094bbba8689f7e
Derrick Pallas <derrick@pallas.us>
2016-11-25 16:29:25 -0800

    stack: make mprotect expression clearer
    The expression for the high guard page was pretty messy.  Instead, adjust
    the stack first then activate the guards.  Additionally, ensure the stack
    size is a multiple of the page size.

--- stack.cc
+++ stack.cc
@@ -29,6 +29,7 @@ namespace {
     ;
 
     static long page_size = sysconf(_SC_PAGE_SIZE);
+    static long page_mask = page_size - 1;
 
     void* adjust(void* p, ptrdiff_t d) {
       return reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(p) + d);
@@ -46,16 +47,19 @@ stack::stack() throw (std::bad_alloc)
 
   ss_size += 2 * page_size;
 
+  const long page_mask = page_size - 1;
+  ss_size += page_mask;
+  ss_size &= ~page_mask;
+
   ss_sp = mmap(NULL, ss_size, stack_prot, stack_flags, -1, 0);
   if (MAP_FAILED == ss_sp)
     throw std::bad_alloc();
 
-
-  TRY(mprotect, ss_sp, page_size, PROT_NONE);
-  TRY(mprotect, adjust(ss_sp, (ss_size-1) & ~(page_size-1)), page_size, PROT_NONE);
-
   ss_sp = adjust(ss_sp, page_size);
   ss_size -= 2 * page_size;
+
+  TRY(mprotect, adjust(ss_sp, -page_size), page_size, PROT_NONE);
+  TRY(mprotect, adjust(ss_sp, ss_size), page_size, PROT_NONE);
 }
 
 stack::~stack() {
